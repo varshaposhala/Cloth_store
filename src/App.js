1b3026751301a8887f3f3f10b4ccd58dbf2ec37e
varshaposhala <varshaposhala046@gmail.com>
2024-12-18 12:05:08 +0530

    Added my project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Homepage from './homepage.component';
+function App() {
+    return (
+        <div className="App">
+            <Homepage />
+        </div>
+    );
+}
+
+export default App;
